70527986844d35298b4d78b59cee132587c6922d
Chris Fowler <chris.fowler73@hotmail.co.uk>
2019-04-27 16:01:54 +0100

    Setup and test connection

---
+++ app.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask, render_template, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'travel_tracker'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI')
+
+mongo = PyMongo(app)
+
+@app.route('/')
+def hello():
+    return 'Hello World'
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT', 5000)),
+            debug=True)
