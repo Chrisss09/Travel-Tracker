dc41ee406ee271869bc97b9ab0d68e31720ccd51
Chris Fowler <chris.fowler73@hotmail.co.uk>
2019-05-01 21:24:27 +0100

    Fixed issue with bug

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app.config["MONGO_DBNAME"] = 'travel_tracker'
 app.config["MONGO_URI"] = os.getenv('MONGO_URI')
 
 mongo = PyMongo(app)
+countries = mongo.db.country.find()
 
 @app.route('/')
 @app.route('/home')
@@ -18,9 +19,12 @@ def index():
 def travel_planner():
     return render_template('planner.html', country=mongo.db.country.find())
 
-@app.route('/travel_planner/<country_id>')
+@app.route('/current_country/<country_id>', methods=['POST', 'GET'])
 def current_country(country_id):
-    return render_template('country.html', country=mongo.db.country.find_one({'_id': ObjectId(country_id)}))
+    #for count in countries.find():
+        #print(count)
+    specific_country = mongo.db.country.find_one({'_id': ObjectId(country_id)})
+    return render_template('country.html', specific_country = specific_country)
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
