87058e254ad4a38ac0f75a0f8cc88f94775ab956
Chris Fowler <chris.fowler73@hotmail.co.uk>
2019-05-01 19:41:52 +0100

    Creating a function for my view button

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
@@ -18,6 +18,9 @@ def index():
 def travel_planner():
     return render_template('planner.html', country=mongo.db.country.find())
 
+@app.route('/travel_planner/<country_id>')
+def current_country(country_id):
+    return render_template('country.html', country=mongo.db.country.find_one({'_id': ObjectId(country_id)}))
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
