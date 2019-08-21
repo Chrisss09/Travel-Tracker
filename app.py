967cc345d2f7eb2ca5b952c3516939474764527f
Chris Fowler <chris.fowler73@hotmail.co.uk>
2019-08-21 18:36:29 +0100

    Fixed bug with insert code

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app.config["MONGO_DBNAME"] = 'travel_tracker'
 app.config["MONGO_URI"] = os.getenv('MONGO_URI')
 
 mongo = PyMongo(app)
-countries = mongo.db.country.find()
+countries = mongo.db.country_post.find()
 hotels = mongo.db.hotel.find()
 
 # Generated map
@@ -94,43 +94,23 @@ def travel_planner():
         flash('Please sign in to add to your planner')
         return render_template('base.html')
     flash('Welcome ' + session['username'] + ', plan and share your journey here.')
-    return render_template('planner.html', country=mongo.db.country.find(), hotel=mongo.db.hotel.find())
+    return render_template('planner.html', country=mongo.db.country_post.find())
 
 @app.route('/current_country/<country_id>', methods=['POST', 'GET'])
 def current_country(country_id):
-    specific_country = mongo.db.country.find_one({'_id': ObjectId(country_id)})
-    specific_hotel = mongo.db.hotel.find_one({'country_name': specific_country['country_name']})
-    return render_template('country.html', specific_country=specific_country, specific_hotel=specific_hotel)
+    specific_country = mongo.db.country_post.find({'_id': ObjectId(country_id)})
+    return render_template('country.html', specific_country=specific_country)
 
 @app.route('/add_country')
 def add_country():
-    return render_template('addcount.html', country=countries, hotel=hotels, rating=mongo.db.rating.find())
+    return render_template('addcount.html', country=countries, rating=mongo.db.rating.find())
 
 @app.route('/confirm_country', methods=['POST'])
 def confirm_country():
     specific_user = mongo.db.user
-    the_country = mongo.db.country
-    the_hotel = mongo.db.hotel
     if specific_user:
-        the_country.insert_many([
-            {
-                'username': session['username'],
-                'country_name':request.form.get('country_name'),
-                'travel_to_date':request.form.get('travel_to_date'),
-                'travel_from_date':request.form.get('travel_from_date'),
-                'flight_time_to':request.form.get('flight_time_to'),
-                'flight_time_from':request.form.get('flight_time_from'),
-                'todo_done':request.form.get('todo_done'),
-                'blog':request.form.get('blog'),
-                'rating_cat':request.form.get('rating_cat')
-            }])
-        the_hotel.insert_many([
-            {
-                'country_name':request.form.get('country_name'),
-                'hotel_name':request.form.get('hotel_name'),
-                'hotel_address':request.form.get('hotel_address'),
-                'hotel_postcode':request.form.get('hotel_postcode')
-            }])
+        the_country = mongo.db.country_post
+        the_country.insert_one(request.form.to_dict())
     return redirect(url_for('travel_planner'))
 
 @app.route('/edit_country/<country_id>')
@@ -221,4 +201,4 @@ def travel_map():
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT', 5000)),
-            debug=False)
+            debug=True)
